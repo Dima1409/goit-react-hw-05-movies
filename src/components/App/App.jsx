a2f11fbe20994b90ef6173e46b9d9ac132d0f31f
Dima <57713733+Dima1409@users.noreply.github.com>
2022-11-15 23:50:25 +0200

    start: add BrowserRouter and basename, add reset CSS, add test Route to HomePage, add homepage in package.json

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import {Routes, Route} from 'react-router-dom';
+
+
+export const App = () => {
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<div>Home start</div>}></Route>
+    </Routes>
+    </>
+  );
+};
